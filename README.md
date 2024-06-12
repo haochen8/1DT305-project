2f0522d730a5ea8c671dfd560de33af8c17f06fe
hao.chen8 <hc222ig@student.lnu.se>
2024-06-12 16:13:17 +0200

    Temperature & Humidity

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# 1DT305-project
+# DHT11 and DHT22 Temperature and Humidity Sensor
+The code prints temperature in Celsius and humidity in percentage from DHT11 and DHT22. Depending on what DHT11 you hold you must choose one of the following connections. If your DHT11 is mounted on a PCB you just connect it to the RPi if it comes with four pins then you need to use a 4.7K ohm resistor for stable reading.
+
+![](connection/DHT11_bb.png)
+
+![](connection/DHT11_Resistor_bb.png)
--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-# Import libraries
-from machine import Pin # import Pin definitions
-import time # import timer library
+import sys
+sys.path.append('lib')
 
-# define GP16 as output pin
-redLED = Pin(16, Pin.OUT)
+import lib.dht as dht
+import machine
+import time
+import urequests
+import lib.wifiConnection as wifiConnection
+import lib.datacake_keys as datacake_keys
 
-# start loop
-while True:    
-    redLED.on() # turn on green LED
-    time.sleep(0.3) # wait 0.3 seconds
-    redLED.off() # turn off green LED
-    time.sleep(0.8) # wait 0.8 second
+# Datacake API details
+DATACAKE_API_URL = "https://api.datacake.co/integrations/api/0f4fbf64-d7b3-42c4-b5aa-9646d7c3d4a2/"
+DATACAKE_TOKEN = ""
+
+tempSensor = dht.DHT11(machine.Pin(27)) # DHT11 Constructor 
+
+# WiFi Connection
+ip = wifiConnection.connect()
+print("Device IP:", ip)
+
+
+while True:
+    try:
+        tempSensor.measure()
+        temperature = tempSensor.temperature()
+        humidity = tempSensor.humidity()
+        print("Temperature is {} degrees Celsius and Humidity is {}%".format(temperature, humidity))
+
+        # Send data to Datacake
+        headers = {
+            'Authorization': 'Token {}'.format(datacake_keys.DATACAKE_TOKEN),
+            'Content-Type': 'application/json'
+        }
+        payload = {
+            'temperature': temperature,
+            'humidity': humidity
+        }
+        response = urequests.post(datacake_keys.DATACAKE_API_URL, headers=headers, json=payload)
+        response.close()
+    except Exception as error:
+        print("Exception occurred", error)
+    time.sleep(15)  # Send updates every 15 seconds
